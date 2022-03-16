3b717b5cd169fc655a9ad0b0afa66ad4581934c5
Lechixy <51778131+Lechixy@users.noreply.github.com>
2022-03-16 21:34:45 +0300

    changing discord link to reaver

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -4,7 +4,8 @@ const socials = {
     steam: `https://steamcommunity.com/id/lechixy/`,
     spotify: `https://open.spotify.com/user/31frempzaphgs26vlu4nn7tpagki?si=43cbe71dd7aa45db`,
     github: `https://github.com/Lechixy`,
-    discord: `https://discord.com/oauth2/authorize?client_id=753906874729889853&permissions=8&scope=bot%20applications.commands`,
+    discord: `https://discord.gg/reaver`,
+    lechsbott: `https://discord.com/oauth2/authorize?client_id=753906874729889853&permissions=8&scope=bot%20applications.commands`,
     snapchat: `https://www.snapchat.com/add/lechixy?share_id=sjO03SDtAOI&locale=en-GB`,
 }
 
