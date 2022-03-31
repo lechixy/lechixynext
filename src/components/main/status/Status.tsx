3f5c7d7920ef91f1e93fb2a8e2c8823f3329bfde
Lechixy <51778131+Lechixy@users.noreply.github.com>
2022-04-01 00:20:05 +0300

    Last commit for this day

--- src/components/main/status/Status.tsx
+++ src/components/main/status/Status.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styles from './Status.module.scss'
 import { Props } from '../../../utils/types';
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Status: FC<Props> = ({ info }) => {
 
@@ -9,7 +10,6 @@ export const Status: FC<Props> = ({ info }) => {
     if (info.data.listening_to_spotify === true) {
 
         let spotify = info.data.spotify
-        console.log(spotify)
         let spotifyact = info.data.activities.find(x => x.type === 2)
 
         return (
@@ -20,15 +20,29 @@ export const Status: FC<Props> = ({ info }) => {
                 <div className={styles.type_2_spotify}>
                     <Image className={styles.type_2_spotify_img} src={spotify.album_art_url} alt={`${spotify.album}`} height={120} width={120} />
                     <div className={styles.type_2_spotify_text}>
-                        <span className={styles.type_2_spotify_text_song}><span className={styles.underline}>{spotify.song}</span></span>
-                        <span className={styles.type_2_spotify_text_artist}>by <span className={styles.underline}>{spotify.artist}</span></span>
-                        <span className={styles.type_2_spotify_text_album}>on <span className={styles.underline}>{spotify.album}</span></span>
+                        <span className={styles.type_2_spotify_text_song}>
+                            <Link href={`https://open.spotify.com/track/${spotify.track_id}`} passHref>
+                                <span className={styles.underline}>{spotify.song}</span>
+                            </Link>
+                        </span>
+                        <span className={styles.type_2_spotify_text_artist}>
+                            by <span className={styles.underline}>{spotify.artist}</span>
+                        </span>
+                        <span className={styles.type_2_spotify_text_album}>
+                            on <span className={styles.underline}>{spotify.album}</span>
+                        </span>
+                        <div>---------</div>
+                        <div>
+                            <span>start</span>
+                            <span>end</span>
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
 
+    //Custom Status
     if (status?.type === 4) {
         return (
             <div className={styles.type_4}>
@@ -38,7 +52,9 @@ export const Status: FC<Props> = ({ info }) => {
         )
     } else {
         return (
-            <span>No status probably doing anything</span>
+            <div className={styles.offline}>
+                <span>*No status probably doing anything*</span>
+            </div>
         )
     }
 }
