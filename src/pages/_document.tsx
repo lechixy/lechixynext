c9cfad71d899ef6544cdcc13a993809b7b7e81b5
Lechixy <51778131+Lechixy@users.noreply.github.com>
2022-05-22 21:40:13 +0300

    Adding meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+                    <meta name="theme-color" content="#574e63"></meta>
+                    <meta property="og:title" content="lechixy"></meta>
+                    <meta property="og:image" content="https://lechixy.netlify.app/favicon.ico"></meta>
                 </Head>
                 <body>
                     <Main />
