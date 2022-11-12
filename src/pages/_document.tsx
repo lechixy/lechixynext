9f7fc6f5e356942c4c1933a7ca98460ad6239bfd
Lechixy <51778131+Lechixy@users.noreply.github.com>
2022-11-12 19:38:57 +0300

    Fixing font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang='en'>
+            <Html lang="en">
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
-                    <meta name="theme-color" content="#574e63"></meta>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <meta name="theme-color" content="#6F00FF"></meta>
                     <meta property="og:title" content="lechixy"></meta>
                     <meta property="og:image" content="https://lechixy.netlify.app/favicon.ico"></meta>
                 </Head>
@@ -17,7 +20,7 @@ class MyDocument extends Document {
                     <Main />
                 </body>
             </Html>
-        )
+        );
     }
 }
 
