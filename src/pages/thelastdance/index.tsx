5b946cdfac9f829ce64bc79ff8543671e704e715
melih <yildizmelihsahin81@gmail.com>
2026-03-19 18:25:36 +0300

    Adding: Video link to thelastdance page

--- src/pages/thelastdance/index.tsx
+++ src/pages/thelastdance/index.tsx
@@ -5,6 +5,10 @@ import styles from "./TheLastDance.module.scss"
 
 const TheLastDance: NextPage = () => {
 
+    function onClickPlay() {
+        window.open("https://photos.app.goo.gl/SpmPvb81NxYiCnyy7", "_blank");
+    }
+
     return (
         <>
             <Head>
@@ -61,13 +65,13 @@ const TheLastDance: NextPage = () => {
                                     Tahtada konu anlatılırken arkada kahkaha tufanı kopardı. Kimsenin tam anlamıyla ne yaptığını bilmediği ama herkesin çok eğlendiği bir sınıftık. Şimdi bu çılgın sınıf final sezonuyla ekranlarda!
                                 </div>
                                 <div className={styles.buttons}>
-                                    <div className={styles.play}>
+                                    <div className={styles.play} onClick={onClickPlay}>
                                         <svg className={styles.playIcon} xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="PlayStandard" aria-hidden="true">
                                             <path fill="currentColor" d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"></path>
                                         </svg>
                                         <div>Oynat</div>
                                     </div>
-                                    <div className={styles.info}>
+                                    <div className={styles.info} onClick={onClickPlay}>
                                         <svg className={styles.infoIcon} xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="CircleIStandard" aria-hidden="true">
                                             <path fill="currentColor" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" clip-rule="evenodd" fill-rule="evenodd"></path>
                                         </svg>
